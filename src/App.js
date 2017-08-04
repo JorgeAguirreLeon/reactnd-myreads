e0881154e22a93e11ba3be091fb03d1874db7670
jorge <jorge.aguirre.leon@gmail.com>
2017-08-04 14:12:46 +0200

    Search input working

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    books: []
+    books: [],
+    search: ''
   }
 
   componentDidMount() {
@@ -25,19 +26,22 @@ class BooksApp extends React.Component {
       <div className="app">
         <Route
           exact path='/'
-          render={()=> {
-            return (
-              <ListBooks
-              wantToRead={this.filterBooksByShelf(this.state.books, 'wantToRead')}
-              currentlyReading={this.filterBooksByShelf(this.state.books, 'currentlyReading')}
-              read={this.filterBooksByShelf(this.state.books, 'read')}
-              />
-            )
-          }}
+          render={()=> (
+            <ListBooks
+            wantToRead={this.filterBooksByShelf(this.state.books, 'wantToRead')}
+            currentlyReading={this.filterBooksByShelf(this.state.books, 'currentlyReading')}
+            read={this.filterBooksByShelf(this.state.books, 'read')}
+            />
+          )}
         />
         <Route
           exact path='/add'
-          render={()=> <SearchBooks />}
+          render={()=> (
+            <SearchBooks
+              search={this.state.search}
+              onSearchChange={e=> this.setState({search: e.target.value})}
+            />
+          )}
         />
       </div>
     )
